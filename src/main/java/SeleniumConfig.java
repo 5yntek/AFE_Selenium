5f26cd504273029d133f93bee240ca855e9c3e4b
ptrckbnck <ptrckbnck@gmail.com>
2018-09-10 21:40:20 +0200

    added laptop geckodriver

--- src/main/java/SeleniumConfig.java
+++ src/main/java/SeleniumConfig.java
@@ -1,4 +1,3 @@
-import com.google.common.collect.Lists;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,7 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class SeleniumConfig {
 
     private WebDriver driver;
-    private static List<String> drivers = List.of("E:\\Java\\geckodriver.exe");
+    private static List<String> drivers = List.of("E:\\Java\\geckodriver.exe",
+            "/home/patrick/Java/geckodriver");
 
     public SeleniumConfig() {
         Optional<String> d = drivers.stream().filter((x) -> Files.exists(Paths.get(x))).findFirst();
